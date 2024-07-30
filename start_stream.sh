444f32612c553718bd1b51dc468f8d45e16056b9
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-07-29 21:33:03 -0400

    Update start_stream.sh

--- start_stream.sh
+++ start_stream.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Define the resolution and frame rate
-RESOLUTION="1920x1080"
-FRAME_RATE="30"
-DEVICE_INDEX="0"  # Updated based on the listed devices
-
-# Stream the display using FFmpeg and send it to the WebSocket server
-ffmpeg -f avfoundation -framerate $FRAME_RATE -i "$DEVICE_INDEX" -vf scale=$RESOLUTION -c:v libvpx -f webm - | websocat ws://192.168.1.41:8765
+# Stream the display using GStreamer
+gst-launch-1.0 -v avfvideosrc device-index=0 ! video/x-raw,framerate=30/1 ! videoconvert ! vp8enc ! webmmux ! tcpserversink host=192.168.1.41 port=5000
