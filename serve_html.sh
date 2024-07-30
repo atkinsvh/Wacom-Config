8e90cb7e2d3b8fb59b6b23093fb95622cdb79110
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-07-29 20:49:30 -0400

    Update serve_html.sh

--- serve_html.sh
+++ serve_html.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# serve_html.sh
-
-# Ensure the HTML file is accessible
-chmod +x index.html
 
 # Serve the HTML file using Python's built-in HTTP server
 PORT=8765
-python3 -m http.server --bind 192.168.1.41 $PORT
-echo "HTML served at http://192.168.1.41:$PORT"
+python3 -m http.server $PORT
