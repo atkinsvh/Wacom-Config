91226cd5b4fcbcf0585696bf8e63f8a10137d0c8
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-07-29 21:26:59 -0400

    Update start_stream.sh

--- start_stream.sh
+++ start_stream.sh
@@ -5,5 +5,5 @@ RESOLUTION="1920x1080"
 FRAME_RATE="30"
 DEVICE_INDEX="0"  # Updated based on the listed devices
 
-# Stream the display using FFmpeg
+# Stream the display using FFmpeg and send it to the WebSocket server
 ffmpeg -f avfoundation -framerate $FRAME_RATE -i "$DEVICE_INDEX" -vf scale=$RESOLUTION -c:v libvpx -f webm - | websocat ws://192.168.1.41:8765
