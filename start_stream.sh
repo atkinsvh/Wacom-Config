8c477fdc5f68eda77dd5764bf2403400fe764d75
Victoria <54598166+atkinsvh@users.noreply.github.com>
2024-07-29 21:13:38 -0400

    Update start_stream.sh

--- start_stream.sh
+++ start_stream.sh
@@ -6,4 +6,4 @@ FRAME_RATE="30"
 DEVICE_INDEX="0"  # Updated based on the listed devices
 
 # Stream the display using FFmpeg
-ffmpeg -f avfoundation -framerate $FRAME_RATE -i "$DEVICE_INDEX" -vf scale=$RESOLUTION -c:v libvpx -f webm udp://192.168.1.41:12345
+ffmpeg -f avfoundation -framerate $FRAME_RATE -i "$DEVICE_INDEX" -vf scale=$RESOLUTION -c:v libvpx -f webm - | websocat ws://192.168.1.41:8765
